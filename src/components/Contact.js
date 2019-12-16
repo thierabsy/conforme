21042264d76bb297cea8d84b6e1614774a157773
thierabsy <thierabsy@yahoo.fr>
2019-12-16 17:32:58 +0100

    Changed icons, remove lazyload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ const Contact = () => {
             <span>Envoyez nous un message</span>
           </div>
           <div className="cfb-form">
-            <form name="Contact" method="POST" data-netlify="true">data-netlify="true"
+            {/* <form name="Contact" method="POST" data-netlify="true">
               <div className="form-control">
                 <label htmlFor="nom">Votre Nom:</label>
                 <input type="text" name="nom" id="nom" required />
@@ -57,6 +57,26 @@ const Contact = () => {
               <div className="btn-submit">
                 <button type="submit">Envoyer</button>
               </div>
+            </form> */}
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>   
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+              <p>
+                <label>Your Role: <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
             </form>
           </div>
           </div>
